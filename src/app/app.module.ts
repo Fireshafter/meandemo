934996aff68fb6dc58d6ea973acd14afc888e913
Fireshafter <fireshafter@hotmail.com>
2020-02-21 10:43:45 +0100

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserNewComponent } from './user/user-new/user-new.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserNewComponent,
+    UserListComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule
